cd72f546355ad92f5f86ff8f3529e9555c607094
Hardi Chandra <hardi.chandra@transferwise.com>
2019-07-07 13:01:33 -0400

    Change server port to 8080, make adjjustment for domain properties

--- src/main/java/com/hardi/taxcalculator/command/CreateTransactionCommand.java
+++ src/main/java/com/hardi/taxcalculator/command/CreateTransactionCommand.java
@@ -3,14 +3,20 @@ package com.hardi.taxcalculator.command;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class CreateTransactionCommand {
+	@NotNull
 	private String name;
+	@NotNull
 	private Integer taxCode;
+	@NotNull
 	private BigDecimal price;
 }
--- src/main/java/com/hardi/taxcalculator/domain/Transaction.java
+++ src/main/java/com/hardi/taxcalculator/domain/Transaction.java
@@ -4,18 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Entity
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 @EqualsAndHashCode
 @Builder
 public class Transaction {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private Integer taxCode;
