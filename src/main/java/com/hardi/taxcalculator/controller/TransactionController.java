ce0d29357b78db2323f5f6299e9a12434c252a38
Hardi Chandra <hardi.chandra@transferwise.com>
2019-07-07 21:18:18 -0400

    Add swagger for API documentation

--- src/main/java/com/hardi/taxcalculator/controller/TransactionController.java
+++ src/main/java/com/hardi/taxcalculator/controller/TransactionController.java
@@ -4,6 +4,8 @@ import com.hardi.taxcalculator.api.command.CreateTransactionCommand;
 import com.hardi.taxcalculator.api.response.TransactionResponse;
 import com.hardi.taxcalculator.domain.Transaction;
 import com.hardi.taxcalculator.service.TransactionService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,18 +22,21 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 @RestController
 @RequestMapping(value = "/v1/transaction")
 @AllArgsConstructor
+@Api(basePath = "/v1/transaction", description = "Transaction API")
 public class TransactionController {
 	
 	@Autowired
 	private TransactionService transactionService;
 
 	@RequestMapping(method = GET)
+	@ApiOperation(value = "get all transactions")
 	public ResponseEntity<List<TransactionResponse>> getTransactions() {
 		List<TransactionResponse> transactionResponses = transactionService.getTransactions();
 		return new ResponseEntity<>(transactionResponses, HttpStatus.OK);
 	}
 
 	@RequestMapping(method = POST)
+	@ApiOperation(value = "create new transaction")
 	public ResponseEntity<TransactionResponse> createTransaction(@RequestBody CreateTransactionCommand createTransactionCommand) {
 		TransactionResponse transactionResponse = transactionService.createTransaction(createTransactionCommand);
 		return new ResponseEntity<>(transactionResponse, HttpStatus.CREATED);
